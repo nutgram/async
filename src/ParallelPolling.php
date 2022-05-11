fac3cd87098e04ee70b50728cd43668623b0fd45
SergiX44 <sergio@brighenti.me>
2022-05-11 23:08:31 +0200

    Initial commit

---
+++ src/ParallelPolling.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SergiX44\Async;
+
+use SergiX44\Nutgram\Nutgram;
+use SergiX44\Nutgram\RunningMode\Polling;
+use Spatie\Async\Pool;
+
+class ParallelPolling extends Polling
+{
+    private Pool $pool;
+
+    public function __construct()
+    {
+        $this->pool = Pool::create();
+    }
+
+    protected function fire(Nutgram $bot, array|null $updates): void
+    {
+        $this->pool->add(function () use ($bot, $updates) {
+            parent::fire($bot, $updates);
+        });
+    }
+
+    public function __destruct()
+    {
+        $this->pool->stop();
+        $this->pool->wait();
+    }
+}
